4b1e324fd70a2f9f69d5fef936a9f82b8536a893
Yusuf Göktaş <91718502+YusufGkts1@users.noreply.github.com>
2022-05-25 16:33:02 +0300

    Project created

---
+++ database/factories/Integration/IntegrationFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Integration;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Integration\Integration>
+ */
+class IntegrationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
